e6adf6a337f67ecdc7e58fbbe96d228751135ec3
olegatorh <herston4ik@gmail.com>
2020-05-02 13:23:36 +0300

    bot

---
+++ baby.py
@@ -0,0 +1,48 @@
+import config
+import telebot
+import requests
+from telebot import types
+
+bot = telebot.TeleBot(config.token)
+
+response = requests.get(config.url).json()
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    itembtn1 = types.KeyboardButton('USD')
+    itembtn2 = types.KeyboardButton('EUR')
+    itembtn3 = types.KeyboardButton('RUR')
+    itembtn4 = types.KeyboardButton('BTC')
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
+    msg = bot.send_message(message.chat.id,
+                    "дізнатись курс ПриватБанка (в відділеннях)", reply_markup=markup)
+    bot.register_next_step_handler(msg, process_coin_step)
+
+def process_coin_step(message):
+    try:
+       markup = types.ReplyKeyboardRemove(selective=False)
+
+       for coin in response:
+           if (message.text == coin['ccy']):
+              bot.send_message(message.chat.id, printCoin(coin['buy'], coin['sale']),
+                               reply_markup=markup, parse_mode="Markdown")
+
+    except Exception as e:
+       bot.reply_to(message, 'ooops!')
+
+def printCoin(buy, sale):
+    '''вивід курсу'''
+    return "💰 *Курс покупки:* " + str(buy) + "\n💰 *Курс продажі:* " + str(sale)
+
+# Enable saving next step handlers to file "./.handlers-saves/step.save".
+# Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
+# saving will hapen after delay 2 seconds.
+bot.enable_save_next_step_handlers(delay=2)
+
+# Load next_step_handlers from save file (default "./.handlers-saves/step.save")
+# WARNING It will work only if enable_save_next_step_handlers was called!
+bot.load_next_step_handlers()
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,2 @@
+token = "1212606305:AAFYUb_kly7Ol4c_FDN1wi0q0LSIE5l0M4k"
+url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
